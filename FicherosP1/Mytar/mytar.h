fc9498604a128517a6f272721d1960effa1f9aeb
Jorge Rodríguez García <jorger09@ucm.es>
2018-09-27 15:23:33 +0200

    primer dia de laboratorio

---
+++ FicherosP1/Mytar/mytar.h
@@ -0,0 +1,20 @@
+#ifndef _MYTAR_H
+#define _MYTAR_H
+
+#include <limits.h>
+
+typedef enum{
+  NONE,
+  ERROR,
+  CREATE,
+  EXTRACT
+} flags;
+
+typedef struct {
+  char* name;
+  unsigned int size;
+} stHeaderEntry;
+
+int createTar(int nFiles, char *fileNames[], char tarName[]);
+
+int extractTar(char tarName[]);
